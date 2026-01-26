54ad8688753809447ef3691cda8f3c7b78feb991
theamazingdp <david.pryor12@gmail.com>
2026-01-26 17:18:17 -0600

    fix: updated to be incremental

--- data/polymarket_analytics/models/silver/btc_prices.sql
+++ data/polymarket_analytics/models/silver/btc_prices.sql
@@ -1,13 +1,19 @@
 -- models/silver/btc_prices.sql
 
 {{ config(
-    materialized='table'
+    materialized='incremental',
+    unique_key='id'
 ) }}
 
 SELECT
+    id,  -- Bronze table has this
     (payload->>'symbol')::varchar as ticker,
     (payload->>'price')::decimal as price,
     (payload->>'volume')::decimal as volume_24h,
     (payload->>'timestamp')::timestamp as price_timestamp,
     ingested_at as discovered_at
-FROM {{ source('bronze', 'btc_prices')}}
+FROM {{ source('bronze', 'btc_prices') }}
+
+{% if is_incremental() %}
+  WHERE ingested_at > (SELECT MAX(discovered_at) FROM {{ this }})
+{% endif %}
